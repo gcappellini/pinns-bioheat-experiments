40f7b1f92a23c6485dbb7d3311d60a703cf580b7
gcappellini <guglielmo.cappellini@uniroma1.it>
2024-06-05 18:32:22 +0200

    next step

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ config["iterations"] = 2
 utils.write_config(config, run)
 a = utils.create_nbho(config)
 
-# Create dataset to test NBHO
+# Create dataset to test NBHO> dolphinx_try
 
 
 # Either train or restore a model, if the config.json is exactly the same. Names of the runs can be different.
