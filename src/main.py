70f07864d635160969d3e67b94f1ad6b30771db3
gcappellini <guglielmo.cappellini@uniroma1.it>
2024-06-08 15:36:08 +0200

    next steps

--- src/main.py
+++ src/main.py
@@ -4,21 +4,24 @@ prj = None
 run = None
 _, output_dir, model_dir, figures_dir = utils.set_name(prj, run)
 
-
+run = "first_try"
 # Create NBHO with some config.json
 config = utils.read_config(run)
 config["iterations"] = 2
 utils.write_config(config, run)
 a = utils.create_nbho(config)
 
-# Create dataset to test NBHO (mathematica, next: dolfinx)
+# Wandb login
+
+# Next:Either train or restore a model, if the config.json is exactly the same. Names of the runs can be different.
+utils.train_model(run)
+
+# Create dataset to test NBHO (next: dolfinx)
 e, f = utils.gen_testdata(1)
 g = utils.gen_obsdata(1)
 
-# Either train or restore a model, if the config.json is exactly the same. Names of the runs can be different.
+# Wandb upload 
 
-
-
-# Plots and metrics
+# Plots
 
 
