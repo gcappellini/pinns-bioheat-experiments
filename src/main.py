080e30e97d75fd92c18f4dde6194d2ae6a1371e6
gcappellini <guglielmo.cappellini@uniroma1.it>
2024-06-10 13:39:35 +0200

    added simulation 3 and 4

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
 import utils
 
 prj = "simulations"
-run = "corrected_bc"
-_, output_dir, model_dir, figures_dir = utils.set_name(prj, run)
+n = [1, 2, 3, 4]
 
+for n_test in n:
+    run = f"single_obs_default_{n_test}"
+    _, output_dir, model_dir, figures_dir = utils.set_name(prj, run)
 
+    # Create NBHO with some config.json
+    config = utils.read_config(run)
+    # config["output_injection_gain"] = 200
+    utils.write_config(config, run)
 
-# Create NBHO with some config.json
-config = utils.read_config(run)
-# config["output_injection_gain"] = 200
-utils.write_config(config, run)
-
-n_test = 1
-utils.single_observer(prj, run, n_test)
+    utils.single_observer(prj, run, n_test)
 
 
 
