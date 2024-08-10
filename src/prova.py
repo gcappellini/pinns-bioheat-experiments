9783919d64992bc9eebb458c291057b7a2d19cb2
gcappellini <guglielmo.cappellini@uniroma1.it>
2024-08-10 10:26:04 +0200

    debug

--- src/prova.py
+++ src/prova.py
@@ -2,14 +2,19 @@ import matlab.engine
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import simulation as ss
 
 current_file = os.path.abspath(__file__)
 src_dir = os.path.dirname(current_file)
 git_dir = os.path.dirname(src_dir)
 
-eng = matlab.engine.start_matlab()
-eng.cd(src_dir, nargout=0)
-eng.simple_script(nargout=0)
-eng.quit()
+# eng = matlab.engine.start_matlab()
+# eng.cd(src_dir, nargout=0)
+# eng.simple_script(nargout=0)
+# eng.quit()
 
+tw1 = ss.calculate_tw1(-0.25, 0.0005)
+print(tw1)
+
+L=0.5
 
--- src/simulation.py
+++ src/simulation.py
@@ -42,8 +42,7 @@ def integrated_pde_x(x, C1, C2):
     return flux, temperature
 
 
-
-def calculate_tfl(yps, ra, ve):
+def calculate_tw1(yps, ra):
 
     hv = 3.66 * k / (2 * ra)
     xw1 = x_tc[2]-ra
@@ -66,6 +65,43 @@ def calculate_tfl(yps, ra, ve):
     C1_solved, C2_solved = solution[C1_v], solution[C2_v]
 
     if isinstance(yps, int):
+        _, theta = integrated_pde_y(yps, C1_solved, C2_solved)
+    if isinstance(yps, float):
+        _, theta = integrated_pde_y(yps, C1_solved, C2_solved)
+    else:
+        thetas = []
+        for el in yps:
+            _, thet = integrated_pde_y(el, C1_solved, C2_solved)
+            thetas.append(thet)
+        theta = np.array(thetas)
+
+    return theta
+
+def calculate_tw2(yps, ra):
+
+    hv = 3.66 * k / (2 * ra)
+    xw2 = x_tc[2]+ra
+    P0_y2 = beta*np.exp(-a*(xw2-x0))*P0
+
+    def integrated_pde_y(y, C1, C2):
+        flux = - P0_y2 * (np.sqrt(np.pi)/2)*special.erf(np.sqrt(b)*y)+C1
+        temperature = (1/k)* (-P0_y2)* (np.sqrt(np.pi)/2)*(np.sqrt(b)*y*special.erf(np.sqrt(b)*y)+np.exp(-b*y**2)/np.sqrt(np.pi)+C2)+C1*y
+        return flux, temperature
+
+    C1_v, C2_v = sp.symbols('C1 C2', real=True)
+    fl_start, t_start = integrated_pde_y(-L/2, C1_v, C2_v)
+    fl_end, t_end = integrated_pde_y(L/2, C1_v, C2_v)
+
+    eq1= sp.Eq(fl_start, hv*(T_fluid_initial-t_start)) 
+    eq2 = sp.Eq(fl_end, hv*(T_fluid_end-t_end))
+
+    # Solve for C1 and C2
+    solution = sp.solve([eq1, eq2], (C1_v, C2_v))
+    C1_solved, C2_solved = solution[C1_v], solution[C2_v]
+
+    if isinstance(yps, int):
+        _, theta = integrated_pde_y(yps, C1_solved, C2_solved)
+    if isinstance(yps, float):
         _, theta = integrated_pde_y(yps, C1_solved, C2_solved)
     else:
         thetas = []
@@ -142,6 +178,39 @@ N = 100  # number of steps
 dy = L / N
 y = np.linspace(-L/2, L/2, N)
 
+def calculate_tfl_distrib(v, R1):
+    # Heat transfer coefficient (h)
+    hv = 3.66 * k / (2 * R1)
+
+    # Arrays to store temperature values
+    T_fluid = np.zeros(N)
+    T_w1 = np.zeros(N)
+    T_w2 = np.zeros(N)
+    Q_in = np.zeros(N)
+    Q_out = np.zeros(N)
+
+    # Set initial values
+    T_fluid[0] = T_fluid_initial
+    T_w1[0] = calculate_tw1(y[0], R1)
+    T_w2[0] = calculate_tw2(y[0], R1)
+    Q_in[0] = hv*(T_w1[0]-T_fluid[0])
+    Q_out[0] = hv*(T_fluid[0]-T_w2[0])
+    
+
+    # Iterative computation
+    for i in range(N - 1):
+        # Update fluid temperature
+        T_fluid[i + 1] = T_fluid[i] + (Q_in[i]-Q_out[i])/(rho*cfl*v)
+        T_w1[i + 1] = calculate_tw1(y[i+1], R1)
+        T_w2[i + 1] = calculate_tw2(y[i+1], R1)
+        Q_in[i + 1] = hv*(T_w1[i + 1]-T_fluid[i + 1])
+        Q_out[i + 1] = hv*(T_fluid[i + 1]-T_w2[i + 1])
+    
+    df = np.vstack((y, T_fluid, T_w1, T_w2, Q_in, Q_out))
+
+    return df
+
+
 # Generate depth values (x) and calculate temperature
 depth = np.linspace(0, 3*L0, 100) 
 
@@ -151,7 +220,7 @@ eng.cd(src_dir, nargout=0)
 eng.simple_script(nargout=0)
 eng.quit()
 a = np.loadtxt(f"{src_dir}/output_pbhe.txt")
-x_values_matlab = a[:, 0]*L0
+x_values_matlab = a[:, 0]*3*L0
 pbhe1_matlab = a[:, 1]
 pbhe2_matlab = a[:, 2]
 pbhe3_matlab = a[:, 3]
@@ -182,14 +251,13 @@ v_vessel2 = 1.5 / 100  # velocity in m/s
 R1_vessel3 = 1.5 / 1000  # radius in meters
 v_vessel3 = 2.0 / 100  # velocity in m/s
 
-temp_vessel1 = calculate_tfl(y, R1_vessel1, v_vessel1)
-temp_vessel2 = calculate_tfl(y, R1_vessel2, v_vessel2)
-temp_vessel3 = calculate_tfl(y, R1_vessel3, v_vessel3)
+temp_vessel1 = calculate_tfl_distrib(R1_vessel1, v_vessel1)
+temp_vessel2 = calculate_tfl_distrib(R1_vessel2, v_vessel2)
+temp_vessel3 = calculate_tfl_distrib(R1_vessel3, v_vessel3)
 
 vessel1 = t_distribution(depth, R1_vessel1, v_vessel1)
 vessel2 = t_distribution(depth, R1_vessel2, v_vessel2)
 vessel3 = t_distribution(depth, R1_vessel3, v_vessel3)
-
 
 
 # Plotting
@@ -232,92 +300,6 @@ plt.legend()
 plt.show()
 
 
-
-
-# # Function to calculate temperature distribution for a vessel
-# def vessel_temperature_distribution(v, R1):
-#     # Heat transfer coefficient (h)
-#     h_flow = 3.66 * k / (2 * R1)
-#     R2 = 2.25/100
-
-#     # Resistances
-#     R_w = 1 / (2 * np.pi * R1 * h_flow * dx)
-#     R_t = np.log(R2 / R1) / (2 * np.pi * k * dx)
-#     R_f = 1 / (np.pi * (R1 ** 2) * v * cfl * rho)
-
-
-#     # Arrays to store temperature values
-#     T_fluid = np.zeros(N)
-#     T_wall = np.zeros(N)
-#     Q = np.zeros(N)
-
-#     # Set initial values
-#     T_fluid[0] = T_fluid_initial
-#     Q[0] = (T_tumour-T_fluid[0])/(R_f+R_w+R_t)
-#     T_wall[0] = T_fluid[0] + Q[0]*R_w
-
-#     # Iterative computation
-#     for i in range(N - 1):
-#         # Update fluid temperature
-#         T_fluid[i + 1] = T_fluid[i] + Q[i]*R_f
-#         Q[i+1] = (T_tumour-T_fluid[i + 1])/(R_f+R_w+R_t)
-#         T_wall[i + 1] = T_fluid[i+1] + Q[i+1]*R_f
-    
-#     df = np.vstack((x, T_fluid, T_wall, Q))
-
-#     return df
-
-
-
-# def temperature_distribution(v, R1, xcc):
-#     r = np.linspace(0, L0, 100) 
-#     # Calculate normalized temperature difference
-#     df1 = vessel_temperature_distribution(v, R1)
-#     x_vals, t_fluid, t_wall, q = df1[0], df1[1], df1[2], df1[3]
-
-#     x_index = np.argmin(np.abs(x_vals - xcc))
-#     T_fluid = t_fluid[x_index]
-#     T_wall = t_wall[x_index]
-#     r_trnsl = r - yv
-#     T_tissue = T_wall + q[x_index] * ((np.log(np.abs(r_trnsl) / R1) / (2 * np.pi * k * dx)))
-
-#     T = np.where(r <= yv - R1, T_fluid_initial +(r/yv)*(T_fluid - T_fluid_initial),
-#                 np.where((yv - R1 < r) & (r <= yv + R1), T_fluid,
-#                         T_tissue))
-#     return T
-
-
-
-# # Calculate temperature distribution for each vessel
-# dd1 = vessel_temperature_distribution(v_vessel1, R1_vessel1, yv)
-# dd2 = vessel_temperature_distribution(v_vessel2, R1_vessel2, yv)
-# dd3 = vessel_temperature_distribution(v_vessel3, R1_vessel3, yv)
-
-# temp_vessel1, temp_vessel2, temp_vessel3 = dd1[1], dd2[1], dd3[1]
-
-# temp_ratio_vessel1 = scale_t(temp_vessel1)
-# temp_ratio_vessel2 = scale_t(temp_vessel2)
-# temp_ratio_vessel3 = scale_t(temp_vessel3)
-
-
-# # Grid settings
-# xgr = 25/100  # x-axis (length along vessel in cm)
-# r = np.linspace(0, L0, 100)  # radial axis (from -1 cm to 1 cm)
-
-# # Calculate normalized temperature difference
-# T_distribution_v1 = temperature_distribution(v_vessel1, R1_vessel1, xgr, yv)
-# T_normalized_v1 = scale_t(T_distribution_v1)
-
-# T_distribution_v2 = temperature_distribution(v_vessel2, R1_vessel2, xgr, yv)
-# T_normalized_v2 = scale_t(T_distribution_v2)
-
-# T_distribution_v3 = temperature_distribution(v_vessel3, R1_vessel3, xgr, yv)
-# T_normalized_v3 = scale_t(T_distribution_v3)
-
-# # print(T_normalized_v1[0], T_normalized_v2[0], T_normalized_v3[0])
-
-
-
 # Plot results
 plt.figure(figsize=(10, 5))
 
@@ -338,29 +320,3 @@ plt.title('Temperature as a Function of Location Along Vessels', fontsize=14)
 plt.grid(True)
 plt.savefig(f"{src_dir}/data/simulations/analytical_along_vessel.png", dpi=120)
 plt.show()
-
-# # Plot the temperature distribution
-# fig = plt.figure(figsize=(12, 8))
-# ax = fig.add_subplot(111)
-# ax.plot(r*100, T_normalized_v1, color='C0', label='Vessel 1', linewidth=1.5)
-# ax.plot(x_values_matlab*100, vessel1_matlab, color='C0', label='Perfusion 1', linewidth=1.2, linestyle="--")
-# ax.plot(r*100, T_normalized_v2, color='C1', label='Vessel 2', linewidth=1.5)
-# ax.plot(x_values_matlab*100, vessel2_matlab, color='C1', label='Perfusion 2', linewidth=1.2, linestyle="--")
-# ax.plot(r*100, T_normalized_v3, color='C2', label='Vessel 3', linewidth=1.5)
-# ax.plot(x_values_matlab*100, vessel3_matlab, color='C2', label='Perfusion 3', linewidth=1.2, linestyle="--")
-
-# # Add 8 equally spaced dashed vertical lines from 0 to 1
-# for i in y_tc:
-#     ax.axvline(100*i, color='r', linestyle='--', linewidth=0.8)
-
-# ax.set_xticks(100 * np.array(y_tc))
-# ax.set_xticklabels([f"{100 * i:.2f}" for i in y_tc], fontsize=10)
-# # Customize the plot
-# ax.set_xlabel('Height, $y$ (cm)', fontsize=12)
-# ax.set_ylabel(r'$\frac{T_{\text{tissue}}(x, r) - T_{\text{fluid}}(0)}{T_{\text{tumour}} - T_{\text{fluid}}(0)}$', fontsize=12)
-# ax.set_title(f'Temperature Distribution Along Height, x={xgr} m', fontsize=14)
-
-# ax.legend()
-# plt.grid(True)
-# plt.savefig(f"{src_dir}/data/simulations/cart_ratio_along_radius.png", dpi=120)
-# plt.show()
