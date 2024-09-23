2763daa4078304f13ca238ed0d58047a8b8b2a42
gcappellini <guglielmo.cappellini@uniroma1.it>
2024-09-24 00:36:02 +0200

    vamos

--- src/prova.py
+++ src/prova.py
@@ -1,11 +1,12 @@
 import os
 import utils as uu
+import common as co
 
 
 current_file = os.path.abspath(__file__)
 src_dir = os.path.dirname(current_file)
 git_dir = os.path.dirname(src_dir)
 models = os.path.join(git_dir, "models")
-
+co.set_prj("training")
 
 uu.mm_observer(8)
