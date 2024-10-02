ad9bbf50b0642e9806e22c19e534c8469f3318bc
gcappellini <guglielmo.cappellini@uniroma1.it>
2024-10-02 19:50:10 +0200

    ready for measurement

--- src/measurements.py
+++ src/measurements.py
@@ -60,34 +60,34 @@ def main():
     # Generate and check observers if needed
     multi_obs = uu.mm_observer(n_obs, config)
 
-    # Import data
-    a = uu.import_testdata()
-    X = a[:, 0:2]
-    meas = a[:, 2:3]
-    x_obs = uu.import_obsdata()
+    # # Import data
+    # a = uu.import_testdata()
+    # X = a[:, 0:2]
+    # meas = a[:, 2:3]
+    # x_obs = uu.import_obsdata()
 
-    lam = config.model_parameters.lam
+    # lam = config.model_parameters.lam
 
-    y1_pred, gt1_pred, gt2_pred, y2_pred = scale_predictions(multi_obs, x_obs, run_figs, lam)
+    # y1_pred, gt1_pred, gt2_pred, y2_pred = scale_predictions(multi_obs, x_obs, run_figs, lam)
 
-    # Load and plot timeseries data
-    file_path = f"{src_dir}/data/vessel/20240522_1.txt"
-    timeseries_data = load_measurements(file_path)
-    df = extract_entries(timeseries_data, 83*60, 4*60*60)
+    # # Load and plot timeseries data
+    # file_path = f"{src_dir}/data/vessel/20240522_1.txt"
+    # timeseries_data = load_measurements(file_path)
+    # df = extract_entries(timeseries_data, 83*60, 4*60*60)
 
 
-    # Optionally check observers and upload to wandb
-    uu.check_observers_and_wandb_upload(multi_obs, x_obs, X, meas, config, output_dir, config)
+    # # Optionally check observers and upload to wandb
+    # uu.check_observers_and_wandb_upload(multi_obs, x_obs, X, meas, config, output_dir, config)
 
-    run_figs = co.set_run(f"mm_obs")
-    config.model_properties.W = None
-    OmegaConf.save(config, f"{run_figs}/config.yaml")
+    # run_figs = co.set_run(f"mm_obs")
+    # config.model_properties.W = None
+    # OmegaConf.save(config, f"{run_figs}/config.yaml")
     
-    # Solve IVP and plot weights
-    uu.solve_ivp_and_plot(multi_obs, run_figs, n_obs, x_obs, X, meas, lam)
+    # # Solve IVP and plot weights
+    # uu.solve_ivp_and_plot(multi_obs, run_figs, n_obs, x_obs, X, meas, lam)
 
-    # Plot time series with predictions
-    pp.plot_timeseries_with_predictions(df, y1_pred, gt1_pred, gt2_pred, y2_pred, run_figs)
+    # # Plot time series with predictions
+    # pp.plot_timeseries_with_predictions(df, y1_pred, gt1_pred, gt2_pred, y2_pred, run_figs)
 
 
 if __name__ == "__main__":
