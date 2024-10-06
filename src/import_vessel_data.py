84e772fa87cbc004f3c3161626e340c29fd29b39
gcappellini <guglielmo.cappellini@uniroma1.it>
2024-10-06 21:55:42 +0200

    import vessel data

--- src/import_vessel_data.py
+++ src/import_vessel_data.py
@@ -1,194 +1,95 @@
 import os
-import datetime
-import pickle
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from utils import scale_t, rescale_time, rescale_x, scale_time
+import utils as uu
+from omegaconf import OmegaConf
+import plots as pp
 
 current_file = os.path.abspath(__file__)
 src_dir = os.path.dirname(current_file)
 
-def parse_line(line):
-    parts = line.strip().split(', ')
-    time_str = parts[0].split()[1]  # Extract the time (HH:MM:SS.microsecond)
-    time = datetime.datetime.strptime(time_str, '%H:%M:%S.%f').time()
-    measurements = parts[2:]  # Skip date and 'Temperature' keyword
-    data = {}
-    for i in range(0, len(measurements), 2):
-        point = int(measurements[i])
-        temperature = float(measurements[i + 1])
-        if point not in data:
-            data[point] = []
-        data[point].append((time, temperature))
-    return data
+def main(meas, rescale, save_pickle, show_y3, threshold):
 
-def load_measurements(file_path):
-    timeseries_data = {}
-    with open(file_path, 'r') as file:
-        for line in file:
-            data = parse_line(line)
-            for point, measurements in data.items():
-                if point not in timeseries_data:
-                    timeseries_data[point] = []
-                timeseries_data[point].extend(measurements)
-    return timeseries_data
+    date = meas.date
+    start_min = meas.start_min
+    end_min = meas.end_min
+    string = meas.string
+    name = meas.title
 
-
-def save_to_pickle(data, file_path):
-    with open(file_path, 'wb') as pkl_file:
-        pickle.dump(data, pkl_file)
-
-def load_from_pickle(file_path):
-    with open(file_path, 'rb') as pkl_file:
-        return pickle.load(pkl_file)
-    
-
-def extract_entries(timeseries_data, tmin, tmax):
-    keys_to_extract = {10: 'y1', 45: 'gt1', 66: 'gt2', 24: 'y2', 31: 'y3', 37:'bol_out'}  # original bol_out: 39
-    extracted_data = {new_key: timeseries_data.get(old_key, []) for old_key, new_key in keys_to_extract.items()}
-
-    # Create a list of all unique times
-    all_times = sorted(set(time for times in extracted_data.values() for time, temp in times))
-    
-    # # Normalize times to seconds, starting from zero
-    start_time = all_times[0]
-    all_times_in_seconds = [(datetime.datetime.combine(datetime.date.today(), time) - 
-                             datetime.datetime.combine(datetime.date.today(), start_time)).total_seconds() 
-                            for time in all_times]
-    
-    # Initialize the dataframe
-    df = pd.DataFrame({'t': np.array(all_times_in_seconds).round()})
-    
-    # Populate the dataframe with temperatures
-    for key, timeseries in extracted_data.items():
-        temp_dict = {time: temp for time, temp in timeseries}
-        df[key] = [temp_dict.get(time, float('nan')) for time in all_times]
-    
-    df = df[(df['t']>tmin) & (df['t']<tmax)].reset_index(drop=True)
-
-    # return df
-    df['time_diff'] = df['t'].diff()#.dt.total_seconds()
-
-    # threshold = np.where((df['t'] > 2 * 60) & (df['t'] < 83 * 60), 50, 1)
-    threshold = 50
-
-    # Identify the indices where a new interval starts
-    new_intervals = df[df['time_diff'] > threshold].index
-
-    # Include the first index as the start of the first interval
-    new_intervals = [0] + list(new_intervals)
-
-    # Create an empty list to store the last measurements of each interval
-    last_measurements = []
-
-    # Extract the last measurement from each interval
-    for i in range(len(new_intervals)):
-        start_idx = new_intervals[i]
-        end_idx = new_intervals[i + 1] - 1 if i + 1 < len(new_intervals) else len(df) - 1
-        last_measurements.append(df.iloc[end_idx])
-
-    # Create the df_short DataFrame from the list of last measurements
-    df_short = pd.DataFrame(last_measurements).drop(columns=['time_diff']).reset_index(drop=True)
-
-    return df_short
-
-
-
-def scale_df(df):
-    time = df['t']-df['t'][0]
-    new_df = pd.DataFrame({'tau': scale_time(time)})
-
-    for ei in ['y1', 'gt1', 'gt2', 'y2', 'y3']:
-        new_df[ei] = scale_t(df[ei])    
-    return new_df
-
-# dates = ["20240522_1", "20240927_1", "20240927_2", "20240927_3", "20240930_1", "20240930_2", "20240930_3" ]
-dates = ["20240930_1"]
-start_min = 5
-end_min = start_min + 30
-name = "heating_meas_3"
-for date in dates:
     file_path = f"{src_dir}/data/vessel/{date}.txt"
 
-    timeseries_data = load_measurements(file_path)
-    df = extract_entries(timeseries_data, start_min*60, end_min*60)
+    timeseries_data = uu.load_measurements(file_path)
+    df = uu.extract_entries(timeseries_data, start_min*60, end_min*60, threshold)
 
-    # print(df)
-    print(df['y1'][0], df['y2'][0], df['y3'][0])
-    df1 = scale_df(df)
-    save_to_pickle(df1, f"{src_dir}/data/vessel/{name}.pkl")
+    # print(df['y1'][0],df['gt1'][0],df['gt2'][0], df['y2'][0], df['y3'][0])
 
-    fig, ax = plt.subplots(figsize=(12, 6))  # Stretching layout horizontally
+    df1 = uu.scale_df(df)
 
-    # Plotting data with specified attributes
-    ax.plot(df['t']/60, df['y1'], label='y1', marker='x')
-    ax.plot(df['t']/60, df['gt1'], label='gt1', alpha=1.0, linewidth=0.7)
-    ax.plot(df['t']/60, df['gt2'], label='gt2', alpha=1.0, linewidth=0.7)
-    ax.plot(df['t']/60, df['y2'], label='y2', alpha=1.0, linewidth=0.7)
-    ax.plot(df['t']/60, df['y3'], label='y3', alpha=1.0, linewidth=0.7)
-    # ax.plot(df['t']/60, df['bol_out'], label='bolus outlet', alpha=1.0, linewidth=0.7)
-
-    # # Add vertical dashed red lines with labels on the plot
-    # # ax.axvline(x=2, color='red', linestyle='--', linewidth=1.1)
-    # # ax.text(2.5, 35.8, 'RF on,\nmax perfusion', color='red', fontsize=10, verticalalignment='top')
-
-    # # ax.axvline(x=29, color='red', linestyle='--', linewidth=1.1)
-    # # ax.text(29.5, 35.8, 'Min perfusion', color='red', fontsize=10, verticalalignment='top')
-
-    # # ax.axvline(x=56, color='red', linestyle='--', linewidth=1.1)
-    # # ax.text(56.5, 35.8, 'Zero perfusion', color='red', fontsize=10, verticalalignment='top')
-
-    # # ax.axvline(x=83, color='red', linestyle='--', linewidth=1.1)
-    # # ax.text(83.5, 35.8, 'RF off, max perfusion', color='red', fontsize=10, verticalalignment='top')
-
-    # Adding legend for the plotted data (excluding the vertical lines)
-    ax.legend()
-
-    # Setting title and labels with modifications
-    # ax.set_title(f"Experiment {date}", fontweight='bold')
-    ax.set_title(f"{date}", fontweight='bold')
-    ax.set_xlabel("Time (min)", fontsize=12)
-    ax.set_ylabel("Temperature (°C)", fontsize=12)
-    # ax.set_xlim(0, 234)
-
-    # Adjust layout for better horizontal stretching
-    plt.tight_layout()
-
-    # Display and save plot
-
-    # plt.savefig(f"{src_dir}/data/vessel/{date}_cooling.png", dpi=120)
-    plt.savefig(f"{src_dir}/data/vessel/{name}.png", dpi=120)
-    plt.show()
-    plt.close()
-
-    # # Plotting scaled data with specified attributes
-    # ax.plot(df1['tau'], df1['y1'], label='y1', marker='x')
-    # ax.plot(df1['tau'], df1['gt1'], label='gt1', alpha=1.0, linewidth=0.7)
-    # ax.plot(df1['tau'], df1['gt2'], label='gt2', alpha=1.0, linewidth=0.7)
-    # ax.plot(df1['tau'], df1['y2'], label='y2', alpha=1.0, linewidth=0.7)
-    # ax.plot(df1['tau'], df1['y3'], label='y3', alpha=1.0, linewidth=0.7)
-    # # ax.plot(df['t']/60, df['bol_out'], label='bolus outlet', alpha=1.0, linewidth=0.7)
+    if save_pickle:
+        uu.save_to_pickle(df1, f"{src_dir}/data/vessel/{string}.pkl")
 
 
-    # # Adding legend for the plotted data (excluding the vertical lines)
-    # ax.legend()
+    title=f"{name}"
+    xlabel="Time (min)"
 
-    # # Setting title and labels with modifications
-    # ax.set_title("Cooling Experiment", fontweight='bold')
-    # ax.set_xlabel(r"$\tau$", fontsize=12)
-    # ax.set_ylabel(r"$\theta$", fontsize=12)
-    # # ax.set_xlim(0, 234)
+    _, _, ylabel = uu.get_scaled_labels(rescale)
 
-    # # Adjust layout for better horizontal stretching
-    # plt.tight_layout()
+    if show_y3:
+        x_vals = np.vstack(([df['t']/60]*5)).T
+        y_vals = np.vstack((df['y1'],df['gt1'],df['gt2'],df['y2'],df['y3'])).T
+        labels = ["y1", "gt1", "gt2", "y2", "y3"]
+        colors = conf.plot.colors.measuring_points
+        linestyles = conf.plot.linestyles.measuring_points
 
-    # # Display and save plot
+    else:
+        x_vals = np.vstack(([df['t']/60]*4)).T
+        y_vals = np.vstack((df['y1'],df['gt1'],df['gt2'],df['y2'])).T
+        labels = ["y1", "gt1", "gt2", "y2"]
+        colors = conf.plot.colors.measuring_points[:-1]
+        linestyles = conf.plot.linestyles.measuring_points[:-1]
 
-    # # plt.savefig(f"{src_dir}/data/vessel/bolus.png", dpi=120)
-    # plt.show()
-    # # plt.close()
-    # # plt.clf()
+
+    pp.plot_generic(x_vals.T, 
+                    y_vals.T, 
+                    title, 
+                    xlabel, 
+                    ylabel, 
+                    labels, 
+                    size = (10, 5),
+                    filename=f"{src_dir}/data/vessel/{string}.png", 
+                    colors=colors, linestyles = linestyles)
+
+
+
+if __name__ == "__main__":
+    conf = OmegaConf.load(f"{src_dir}/config.yaml")
+    rescale = conf.plot.rescale
+    save_pickle = conf.experiment.save_pickle
+
+    # Loop through all experiment types except antenna_characterization
+    for experiment_type in conf.experiment.type.keys():
+        
+        if experiment_type == 'antenna_characterization':
+            antenna_exp = conf.experiment.type.antenna_characterization
+            threshold = antenna_exp['threshold']
+            show_y3 = antenna_exp['show_y3']
+            main(antenna_exp, rescale, save_pickle, show_y3, threshold)  # Use the extracted values
+            continue
+
+        # For other experiment types
+        experiment_group = getattr(conf.experiment.type, experiment_type)
+        threshold = experiment_group['threshold']
+        show_y3 = experiment_group['show_y3']
+
+        # Process each measurement in the experiment group
+        for meas_key in experiment_group.keys():
+            if meas_key.startswith('meas_'):
+                meas = getattr(experiment_group, meas_key)
+                main(meas, rescale, save_pickle, show_y3, threshold)
+
+
+
+
+
 
 
     
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,14 @@
 import deepxde as dde
 import numpy as np
 import os
-import matplotlib.pyplot as plt
 import torch
-import seaborn as sns
 import wandb
 from scipy.interpolate import interp1d
 from scipy import integrate
 import pickle
+import datetime
 import pandas as pd
 import coeff_calc as cc
-import hashlib
 import plots as pp
 import common as co
 from omegaconf import OmegaConf
@@ -728,3 +726,93 @@ def solve_ic_comp(y1_0, y2_0, y3_0, K, a5):
     b_2 = b_3 + K * (b_3 + b_4) + a5 * (y3_0 - y2_0) + K * y2_0
     
     return b_2, b_3, b_4
+
+
+def parse_line(line):
+    parts = line.strip().split(', ')
+    time_str = parts[0].split()[1]  # Extract the time (HH:MM:SS.microsecond)
+    time = datetime.datetime.strptime(time_str, '%H:%M:%S.%f').time()
+    measurements = parts[2:]  # Skip date and 'Temperature' keyword
+    data = {}
+    for i in range(0, len(measurements), 2):
+        point = int(measurements[i])
+        temperature = float(measurements[i + 1])
+        if point not in data:
+            data[point] = []
+        data[point].append((time, temperature))
+    return data
+
+def load_measurements(file_path):
+    timeseries_data = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            data = parse_line(line)
+            for point, measurements in data.items():
+                if point not in timeseries_data:
+                    timeseries_data[point] = []
+                timeseries_data[point].extend(measurements)
+    return timeseries_data
+
+
+def save_to_pickle(data, file_path):
+    with open(file_path, 'wb') as pkl_file:
+        pickle.dump(data, pkl_file)
+
+def load_from_pickle(file_path):
+    with open(file_path, 'rb') as pkl_file:
+        return pickle.load(pkl_file)
+    
+
+def extract_entries(timeseries_data, tmin, tmax, threshold):
+    keys_to_extract = {10: 'y1', 45: 'gt1', 66: 'gt2', 24: 'y2', 31: 'y3', 37:'bol_out'}  # original bol_out: 39
+    extracted_data = {new_key: timeseries_data.get(old_key, []) for old_key, new_key in keys_to_extract.items()}
+
+    # Create a list of all unique times
+    all_times = sorted(set(time for times in extracted_data.values() for time, temp in times))
+    
+    # # Normalize times to seconds, starting from zero
+    start_time = all_times[0]
+    all_times_in_seconds = [(datetime.datetime.combine(datetime.date.today(), time) - 
+                             datetime.datetime.combine(datetime.date.today(), start_time)).total_seconds() 
+                            for time in all_times]
+    
+    # Initialize the dataframe
+    df = pd.DataFrame({'t': np.array(all_times_in_seconds).round()})
+    
+    # Populate the dataframe with temperatures
+    for key, timeseries in extracted_data.items():
+        temp_dict = {time: temp for time, temp in timeseries}
+        df[key] = [temp_dict.get(time, float('nan')) for time in all_times]
+    
+    df = df[(df['t']>tmin) & (df['t']<tmax)].reset_index(drop=True)
+
+    # return df
+    df['time_diff'] = df['t'].diff()#.dt.total_seconds()
+
+    # Identify the indices where a new interval starts
+    new_intervals = df[df['time_diff'] > threshold].index
+
+    # Include the first index as the start of the first interval
+    new_intervals = [0] + list(new_intervals)
+
+    # Create an empty list to store the last measurements of each interval
+    last_measurements = []
+
+    # Extract the last measurement from each interval
+    for i in range(len(new_intervals)):
+        start_idx = new_intervals[i]
+        end_idx = new_intervals[i + 1] - 1 if i + 1 < len(new_intervals) else len(df) - 1
+        last_measurements.append(df.iloc[end_idx])
+
+    # Create the df_short DataFrame from the list of last measurements
+    df_short = pd.DataFrame(last_measurements).drop(columns=['time_diff']).reset_index(drop=True)
+
+    return df_short
+
+def scale_df(df):
+    time = df['t']-df['t'][0]
+    new_df = pd.DataFrame({'tau': scale_time(time)})
+
+    for ei in ['y1', 'gt1', 'gt2', 'y2', 'y3']:
+        new_df[ei] = scale_t(df[ei])    
+    return new_df
