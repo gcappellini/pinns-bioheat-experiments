0c9af831f6edb41fbb824bff859fad8b43fb3a72
gcappellini <cappellini@diag.uniroma1.it>
2024-10-10 12:19:18 +0200

    after training

--- src/prova.py
+++ src/prova.py
@@ -1,10 +1,15 @@
 import numpy as np
 import utils as uu
 import os
+import subprocess
+from omegaconf import OmegaConf
 current_file = os.path.abspath(__file__)
 src_dir = os.path.dirname(current_file)
 
+subprocess.run(["python", f'{src_dir}/main.py'])
 
-b = np.linspace(0.20675, 0.2435, num=8).round(5)
+cfg = OmegaConf.load(f"{src_dir}/config.yaml")
+cfg.experiment.name = ["cooling", "meas_2"]
+OmegaConf.save(cfg, f"{src_dir}/config.yaml")
 
-print(b)
+subprocess.run(["python", f'{src_dir}/main.py'])
