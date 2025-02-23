52a17a8f2f920f5c209c72cf570591f2cff660f5
gcappellini <cappellini@diag.uniroma1.it>
2025-02-23 19:40:33 +0100

    ok

--- src/import_wandb_res.py
+++ src/import_wandb_res.py
@@ -11,84 +11,83 @@ conf_dir = os.path.join(src_dir, "configs")
 models = os.path.join(git_dir, "models")
 tests_dir = os.path.join(git_dir, "tests")
 
-# api = wandb.Api()
+api = wandb.Api()
 
-# # Project is specified by <entity/project-name>
-# runs = api.runs("ht-pinns/2025-02-23_opt_sp_direct")
+# Project is specified by <entity/project-name>
+runs = api.runs("ht-pinns/2025-02-23_opt_sp_direct_1")
 
-# summary_list, config_list, name_list = [], [], []
-# for run in runs: 
-#     # .summary contains the output keys/values for metrics like accuracy.
-#     #  We call ._json_dict to omit large files 
-#     summary_list.append(run.summary._json_dict)
-#     # Keep only the specified keys in each summary dictionary
-#     keys_to_keep = ["L2RE", "MSE", "_runtime", "max", "mean", "test"]
-#     filtered_summary_list = [{k: v for k, v in summary.items() if k in keys_to_keep} for summary in summary_list]
-#     summary_list = filtered_summary_list
+summary_list, config_list, name_list = [], [], []
+for run in runs: 
+    # .summary contains the output keys/values for metrics like accuracy.
+    #  We call ._json_dict to omit large files 
+    summary_list.append(run.summary._json_dict)
+    # Keep only the specified keys in each summary dictionary
+    keys_to_keep = ["L2RE", "MSE", "runtime", "max", "mean", "testloss"]
+    filtered_summary_list = [{k: v for k, v in summary.items() if k in keys_to_keep} for summary in summary_list]
+    summary_list = filtered_summary_list
 
-#     # .config contains the hyperparameters.
-#     #  We remove special values that start with _.
-#     config_list.append(
-#         {k: v for k,v in run.config.items()
-#           if not k.startswith('_')})
+    # .config contains the hyperparameters.
+    #  We remove special values that start with _.
+    config_list.append(
+        {k: v for k,v in run.config.items()
+          if not k.startswith('_')})
 
-#     # .name is the human-readable name of the run.
-#     name_list.append(run.name)
+    # .name is the human-readable name of the run.
+    name_list.append(run.name)
 
 
     
-# # Flatten the summary and config dictionaries and merge them into a single dictionary for each run
-# flattened_data = []
-# for summary, config, name in zip(summary_list, config_list, name_list):
-#     flattened_dict = {**config, **summary}
-#     flattened_data.append(flattened_dict)
+# Flatten the summary and config dictionaries and merge them into a single dictionary for each run
+flattened_data = []
+for summary, config, name in zip(summary_list, config_list, name_list):
+    flattened_dict = {**config, **summary}
+    flattened_data.append(flattened_dict)
 
-# # Create a DataFrame from the flattened data
-# runs_df = pd.DataFrame(flattened_data)
-# # Order the DataFrame by the 'test' column from lower to greater
-# runs_df.rename(columns={'_runtime': 'runtime', 'MSE':'mse','num_domain':'nres', 'num_boundary':'nb'}, inplace=True)
-# runs_df = runs_df.sort_values(by='loss test', ascending=True)
-# cols = runs_df.columns.tolist()
-# new_cols = ['nres','nb','resampling','runtime','testloss','mse']
-# runs_df = runs_df[new_cols]
+# Create a DataFrame from the flattened data
+runs_df = pd.DataFrame(flattened_data)
+# Order the DataFrame by the 'test' column from lower to greater
+runs_df.rename(columns={'MSE':'mse','num_domain':'nres', 'num_boundary':'nb'}, inplace=True)
+cols = runs_df.columns.tolist()
+new_cols = ['nres','nb','resampling','runtime','testloss','mse']
+runs_df = runs_df[new_cols]
 
-# pd.options.display.float_format = '{:.1e}'.format
-# runs_df["runtime"] = runs_df["runtime"].apply(lambda x: f"{x:.1f}")
-# runs_df = runs_df.sort_values(by='loss_test', ascending=True)
+pd.options.display.float_format = '{:.1e}'.format
+runs_df["runtime"] = runs_df["runtime"].apply(lambda x: f"{x:.1f}")
+runs_df = runs_df.sort_values(by='testloss', ascending=True)
 out_dir = f"{tests_dir}/wandb_results"
-# os.makedirs(out_dir, exist_ok=True)
-# runs_df.to_csv(f"{out_dir}/sp_direct.csv", index=False)
+os.makedirs(out_dir, exist_ok=True)
+runs_df.to_csv(f"{out_dir}/sp_direct_1.csv", index=False)
 
 
 
-# Load YAML file
-with open(f"{conf_dir}/pinn_hp/config_direct.yaml", "r") as file:  # Change filename if needed
-    data = yaml.safe_load(file)
-    # Rename keys in data
-    key_mapping = {
-        "activation": "af",
-        "initial_weights_regularizer": "iwr",
-        "initialization": "init",
-        "learning_rate": "lr",
-        "num_dense_layers": "depth",
-        "num_dense_nodes": "width",
-        "num_domain": "nres",
-        "num_boundary": "nb",
-        "num_test": "ntest",
-        "w_res": "wres",
-        "w_bc0": "wbc0",
-        "n_ins": "nins",
-        "n_anchor_points": "nanc"
-    }
+# # Load YAML file
+# with open(f"{conf_dir}/pinn_hp/config_direct.yaml", "r") as file:  # Change filename if needed
+#     data = yaml.safe_load(file)
+#     # Rename keys in data
+#     key_mapping = {
+#         "activation": "af",
+#         "initial_weights_regularizer": "iwr",
+#         "initialization": "init",
+#         "learning_rate": "lr",
+#         "num_dense_layers": "depth",
+#         "num_dense_nodes": "width",
+#         "num_domain": "nres",
+#         "num_boundary": "nb",
+#         "num_test": "ntest",
+#         "w_res": "wres",
+#         "w_bc0": "wbc0",
+#         "n_ins": "nins",
+#         "n_anchor_points": "nanc"
+#     }
 
-    # Remove underscores from other entries
-    data = {key_mapping.get(k, k.replace('_', '')): v for k, v in data.items()}
-# Write to CSV file
-csv_filename = f"{out_dir}/config_direct.csv"
-with open(csv_filename, "w", newline="") as file:
-    writer = csv.writer(file)
-    for key, value in data.items():
-        writer.writerow([key, value])
+#     # Remove underscores from other entries
+#     data = {key_mapping.get(k, k.replace('_', '')): v for k, v in data.items()}
+# # Write to CSV file
+# csv_filename = f"{out_dir}/config_direct.csv"
+# with open(csv_filename, "w", newline="") as file:
+#     writer = csv.writer(file)
+#     for key, value in data.items():
+#         writer.writerow([key, value])
 
 
 
